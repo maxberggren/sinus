c90448e64ebc2bd93cf037d84b6083c15041e210
Max Berggren <maxberggren@gmail.com>
2014-10-20 11:10:09 +0200

    morr

--- textLoc26.py
+++ textLoc26.py
@@ -136,7 +136,7 @@ class tweetLoc:
         
         # Hämta koordinater som har ordet i tweeten eller i metadatan
         # Metadata är användarens självspecifierade ort ex. "svettiga svedala" 
-        result = self.tweetsdb.query("SELECT * FROM tweets WHERE tweet LIKE '%" + word + "%' or metadata LIKE '%" + word + "%' and used = " + str(used) + limit)
+        result = self.tweetsdb.query("SELECT * FROM tweets WHERE tweet LIKE '%inte%'")
         for row in result:
             outputCoordinates.append([row['lon'], row['lat']])
         
