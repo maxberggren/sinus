9fe8de62bf9f9d23d84915091a5b6a108b7df225
Max Berggren <maxberggren@gmail.com>
2014-10-21 15:15:53 +0200

    compile fix

--- textLoc26.py
+++ textLoc26.py
@@ -128,16 +128,9 @@ class tweetLoc:
         # Hämta koordinater som har ordet i tweeten eller i metadatan
         # Metadata är användarens självspecifierade ort ex. "svettiga svedala" 
         
-        sql = "SELECT * FROM tweets WHERE tweet LIKE '%s' or metadata LIKE '%s' and used = 0" 
-        params = ('%' + word + '%', '%' + word + '%',)
-        #print sql % params
-        result = self.db.query(sql, params)
-        
-        #q = "SELECT * FROM tweets WHERE tweet LIKE '%{}%' or metadata LIKE '%{}%' and used = 0".format(word, word)
-        #print q
-        #table = self.db['tweets'].table
-        #q = table.select(table.c.tweet.like('%{}%'.format(word)))
-        #result = self.db.query(q)
+        q = "SELECT * FROM tweets WHERE tweet LIKE '%{}%' and used = 0".format(word)
+        print q
+        result = self.db.query(q)
 
         for row in result:
             outputCoordinates.append([row['lon'], row['lat']])
