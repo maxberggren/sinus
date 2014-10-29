6782b8a588831f9bbb25f57b927d3ad0ee8e1c2d
Max Berggren <maxberggren@gmail.com>
2014-10-29 14:20:31 +0100

    annotating my code

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -558,7 +558,7 @@ def site(urlSearch=None):
     stats = {}
     
     # Get sourcecount for sources that have lon lat
-    key = "sourceswithlatlon3"
+    key = "sourceswithlatlon4"
     if not cache.get(key):
         stats[key] = []
         result = mysqldb.query("SELECT source, rank, COUNT(*) as count FROM blogs "
@@ -572,7 +572,7 @@ def site(urlSearch=None):
         stats[key] = cache.get(key)
     
     # Get sourcecount for sources that yet have no lat lon
-    key = "sourceswithoutlatlon3"
+    key = "sourceswithoutlatlon4"
     if not cache.get(key):
         stats[key] = []
         result = mysqldb.query("SELECT source, rank, COUNT(*) as count FROM blogs "
