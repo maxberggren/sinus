873cb874049360448422b5e212b5dc1f2d9ffa78
Max Berggren <maxberggren@gmail.com>
2014-11-03 10:18:04 +0100

    vectorizing code

--- textLoc26.py
+++ textLoc26.py
@@ -259,7 +259,7 @@ class tweetLoc:
                 for row in result:
                     subscores = np.append(subscores, [row['scoring']])
                     latlon = np.asarray([row['lat'], row['lon']])
-                    subcoordinates = np.concatenate((subcoordinates, [latlon])) 
+                    subcoordinates = np.concatenate((subcoordinates, latlon), axis=1) 
                     
                     
                     freqInBatch = row['n_coordinates']
