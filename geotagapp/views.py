79c51cf2b161d5c633a630ee93616684facae629
Max Berggren <maxberggren@gmail.com>
2014-11-03 13:45:58 +0100

    api for getting training data to jussi

--- geotagapp/views.py
+++ geotagapp/views.py
@@ -79,6 +79,7 @@ def getData(ammountData=None, random=0):
         blogtext = ""                  
         for postrow in mysqldb.query("SELECT * from posts "
                                      "WHERE blog_id = " + str(blogrow['id'])):
+            print type(postrow['text'])
             try:
                 blogtext += "\n\n" + postrow['text'].encode('utf-8')
             except UnicodeDecodeError:
