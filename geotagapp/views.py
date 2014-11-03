7a098c0386caad811c99be3eded791d64526abfe
Max Berggren <maxberggren@gmail.com>
2014-11-03 13:57:30 +0100

    api for getting training data to jussi

--- geotagapp/views.py
+++ geotagapp/views.py
@@ -83,7 +83,7 @@ def getData(ammountData=None, random=0):
             print type(postrow['text'])
             print postrow
             #try:
-            blogtext += "\n\n" + postrow['text'].encode('latin-1')
+            blogtext += "\n\n" + postrow['text'].decode('latin-1')
             #except UnicodeDecodeError:
             #    pass
             
