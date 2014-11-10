09277b0b5f701d7ba69dd9d92f4b385fa9f164c6
Max Berggren <maxberggren@gmail.com>
2014-11-10 08:28:06 +0100

    fixing bug that probablys has to do with the vectorization

--- tagData.py
+++ tagData.py
@@ -137,6 +137,7 @@ if __name__ == "__main__":
             text = u""
             for post in posts:
                 text = text + u"\n\n" + maxFix(post['text'])
+            text = text.encode('utf-8')
             
             print "Belägger " + row['url'] + "..."
             print text.encode('utf-8')
