42670da3a87b889ea7614f5b7dc69e8d843a8596
Max Berggren <maxberggren@gmail.com>
2014-11-17 13:25:46 +0100

    starting to make new prediction modules for the geotagger

--- evalMaxModel.py
+++ evalMaxModel.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
         predictedCoordinateT1, scoreT1, mostUsefulWordsT1, mentionsT1 = data
         
         # Test 2: röstningsförfarandet
-        data2 = predictViaAPI(text, path="vote1")
+        data2 = predictViaAPI(text, path="tagbyvote1")
         predictedCoordinateT2, scoreT2, mostUsefulWordsT2, mentionsT2 = data2
     
         # Test 1
