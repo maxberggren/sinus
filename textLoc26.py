2bab4d26d88a19df74ba57dbceb9f2503107a459
Max Berggren <maxberggren@gmail.com>
2014-11-18 15:12:39 +0100

    starting to make new prediction modules for the geotagger

--- textLoc26.py
+++ textLoc26.py
@@ -502,7 +502,7 @@ class tweetLoc:
                 c.update(found)
 
         text = " ".join([t[0] for t in c.most_common(30)])
-        print "hej"
+        print text
         return self.predict(text)
         
 
