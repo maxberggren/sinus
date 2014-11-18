1b03dfd92e74666e439d9c03bdb19019a5633822
Max Berggren <maxberggren@gmail.com>
2014-11-18 12:56:19 +0100

    starting to make new prediction modules for the geotagger

--- textLoc26.py
+++ textLoc26.py
@@ -495,9 +495,9 @@ class tweetLoc:
         
         patterns = codecs.open("ortgrammatik.txt", encoding="utf-8")
         text = text.lower().decode('utf-8')
-        print type(text)
-        print text
         for pattern in self.patterns:
+            print pattern
+            print text
             found = re.findall(pattern, text)
             if found:
                 print found
