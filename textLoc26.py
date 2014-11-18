65a6e05a9a7d6d3953e4996561af1af40171af41
Max Berggren <maxberggren@gmail.com>
2014-11-18 14:19:48 +0100

    starting to make new prediction modules for the geotagger

--- textLoc26.py
+++ textLoc26.py
@@ -59,7 +59,7 @@ class tweetLoc:
         
         self.words = []
         
-        patterns = codecs.open("ortgrammatik.txt", encoding="utf-8")
+        patterns = codecs.open("ortgrammatik_kort.txt", encoding="utf-8")
         self.patterns = []
         for pattern in patterns:
             pattern = pattern.strip().replace("**PLATS**", "(\S{2,30})")
