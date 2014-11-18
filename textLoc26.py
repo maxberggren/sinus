90e6970b87996429425c28513ae0db2b87b5ab3b
Max Berggren <maxberggren@gmail.com>
2014-11-18 15:45:31 +0100

    starting to make new prediction modules for the geotagger

--- textLoc26.py
+++ textLoc26.py
@@ -289,7 +289,7 @@ class tweetLoc:
             
             # Vikta samman batcharna. TODO: fallande vikt efter datum
             coordinate, score = self.weightedMean(batchcoordinates, batchscores)    
-            
+            print score, threshold
             if score > threshold:
                 coordinates.append(coordinate)
                 scores.append(score)
