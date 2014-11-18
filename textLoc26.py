cfc154aeef9bbd2a0d37fc6e938351a823e49fb5
Max Berggren <maxberggren@gmail.com>
2014-11-18 14:25:04 +0100

    starting to make new prediction modules for the geotagger

--- textLoc26.py
+++ textLoc26.py
@@ -501,7 +501,7 @@ class tweetLoc:
             if found:
                 c.update(found)
 
-        print c.most_common(10)
+        print c.most_common(20)
         
         #return coordinate, score, {}, 0, {}
         
