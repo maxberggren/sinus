76da46c67bee8b70a093ae48763ded71a88ae5bb
Max Berggren <maxberggren@gmail.com>
2014-11-24 13:54:28 +0100

    finding out wich grammars are teh best

--- createGrammar.py
+++ createGrammar.py
@@ -230,7 +230,7 @@ if __name__ == "__main__":
             #print regexpes[regexpscores > 0]
             
             sorted = np.sort(np.vstack((regexpes, regexpscores)), axis=1)
-            print sorted
+            print sorted[0,sorted[0,:] > 0]
             
         except KeyboardInterrupt:
             print "Avslutar"
