7263dda2e73cb7940305863151f23bbc820da1f2
Max Berggren <maxberggren@gmail.com>
2014-11-25 09:03:42 +0100

    finding out wich grammars are teh best

--- createGrammar.py
+++ createGrammar.py
@@ -166,9 +166,6 @@ if __name__ == "__main__":
                     ngramsAround.update(around)
         
         top = 200  
-        before, _ = zip(*ngramsBefore.most_common(top))
-        #after, _ = ngramsAfter.most_common(top)
-        #around, _ = ngramsAround.most_common(top)
             
         for utterance, frq in ngramsBefore.most_common(top):
             if len(utterance) > 0: 
