c8f75288128374b74e482de6d64b4cb7179d0c33
Max Berggren <maxberggren@gmail.com>
2014-12-01 10:40:31 +0100

    finding out wich grammars are teh best

--- textLoc26.py
+++ textLoc26.py
@@ -579,7 +579,7 @@ class tweetLoc:
             if found:
                 c.update(found)
 
-        text = " ".join([t[0] for t in c.most_common()])
+        text = " ".join([t[0] for t in c.most_common() if t[1] > 5])
                 
         return self.predict(text, threshold=threshold)
 
