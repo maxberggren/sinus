32d5014e86a4ca8ecdf19c4db24da4a3ad929f8e
Max Berggren <maxberggren@gmail.com>
2014-12-08 14:33:05 +0100

    test 4 mange

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -214,10 +214,13 @@ def genImages(coordinatesByWord, xBins, words, zoom,
     if dates: # same as a gif should be created
         
         ts = [{'date':str(date),'value':value} for date, value in zip(dates, coordinatesByWord[0])]
-        print ts
+        dates = []
         
         for k, v in groupby(ts, key=lambda x:x['date'][:7]):
-            print k, v['value']
+            print k, list(v)
+            chunkdates = [c['value'] for c in list(v)]
+            dates.append(chunkdates)
+        print dates
             
         dates = np.array_split(dates, chunks)
 
