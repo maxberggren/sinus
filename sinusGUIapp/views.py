cf85b2675b9405123f642559089e641d4e8e3490
Max Berggren <maxberggren@gmail.com>
2014-12-08 14:42:42 +0100

    test 4 mange

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -218,9 +218,9 @@ def genImages(coordinatesByWord, xBins, words, zoom,
         
         for k, v in groupby(ts, key=lambda x:x['date'][:7]):
             #print k, list(v)
-            for c in list(v):
-                print c['value']
             #dates.append(chunkdates)
+            
+            print [c['value'] for c in list(v)]
             #print chunkdates
             
         dates = np.array_split(dates, chunks)
