0771e2f1de7dfb99b321387112cf9eaf4789dda5
Max Berggren <maxberggren@gmail.com>
2015-01-13 11:03:21 +0100

    T11

--- GMMtop.py
+++ GMMtop.py
@@ -62,3 +62,4 @@ if __name__ == "__main__":
         gmmWord = row['word'].encode('utf-8')
         if "@" not in gmmWord and "#" not in gmmWord and gmmWord not in o:
             print "#{i:>7} {word}".format(i=i, word=gmmWord)
+            i += 1
