cd6f15fa04fbd240ee2bc0a281a0000d9bc5c6c7
Max Berggren <maxberggren@gmail.com>
2015-01-13 10:35:43 +0100

    T11

--- GMMtop.py
+++ GMMtop.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     # Hello mr DB
     db = dataset.connect(c.LOCATIONDB+ "?charset=utf8")
     db.query("set names 'utf8'")
-    result = db.query("SELECT * from GMMs order by scoring desc LIMIT 10000")
+    result = db.query("SELECT * from GMMs WHERE n_coordinates > 100 order by scoring desc LIMIT 10000")
     
     for row in result:
         print row['word'].encode('utf-8'), row['scoring'], row['n_coordinates']
