3b0b46eefc6ec8b2de60435e517592b21588630a
Max Berggren <maxberggren@gmail.com>
2015-01-18 20:31:16 +0100

    tweetmap

--- textLoc26.py
+++ textLoc26.py
@@ -190,34 +190,32 @@ class tweetLoc:
         wordsWithModelAccepted = []
         # Skapar en GMM för alla ord
         for i, word in enumerate(words):
-            #try:                  
-            coordinateData = self.getCoordinatesFor(word)
-            if len(coordinateData) > 50:
-                
-
-                myGMM = mixture.GMM(n_components=1)
-                myGMM.fit(np.asarray(coordinateData)) # sklearn wants nparray
-                
-                for coordinate in myGMM.means_: # en GMM tar fram 3 toppar
-                    # Empiriskt satt score
-                    scoring = np.exp(-100 / myGMM.score([coordinate]))[0]   
+            try:                  
+                coordinateData = self.getCoordinatesFor(word)
+                if len(coordinateData) > 50:
                     
-                    # In met i databasen         
-                    self.db['GMMs'].insert(dict(word=word, 
-                                                lon=coordinate[0], 
-                                                lat=coordinate[1], 
-                                                scoring=scoring,
-                                                date=datetime.date.today(),
-                                                n_coordinates=len(coordinateData),
-                                                gaussians=1))
-                    print str(i) + "/" + str(len(words)) + " " + word
 
-                del myGMM 
-                wordsWithModelAccepted.append(word)     
-            #except:
-            #    """
-            #    Ordet kunde inte skapas en GMM på
-            #    """
+                    myGMM = mixture.GMM(n_components=1)
+                    myGMM.fit(np.asarray(coordinateData)) # sklearn wants nparray
+                    
+                    for coordinate in myGMM.means_: # en GMM tar fram 3 toppar
+                        # Empiriskt satt score
+                        scoring = np.exp(-100 / myGMM.score([coordinate]))[0]   
+                        
+                        # In met i databasen         
+                        self.db['GMMs'].insert(dict(word=word, 
+                                                    lon=coordinate[0], 
+                                                    lat=coordinate[1], 
+                                                    scoring=scoring,
+                                                    date=datetime.date.today(),
+                                                    n_coordinates=len(coordinateData),
+                                                    gaussians=1))
+                        print str(i) + "/" + str(len(words)) + " " + word
+
+                    del myGMM 
+                    wordsWithModelAccepted.append(word)     
+            except:
+                print "For {} no GMM was created".format(word)
 
         # Kasta gamla använda tweets
         #result = self.db.query("DELETE from tweets WHERE used = 1")
