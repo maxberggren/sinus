91a9602a22d39e0eacb3e2ebae7077c77b541461
Max Berggren <maxberggren@gmail.com>
2015-01-20 06:43:52 +0100

    tweetmap

--- evalModel_T4.py
+++ evalModel_T4.py
@@ -21,7 +21,7 @@ Test 4
 
 Testa mot grammatiken som laddas fr textfil.
 python evalModel_T4.py 2>&1 | tee -a T_Grammar_Centroid_1e40.log
-python evalModel_T4.py 2>&1 | tee -a T_2Gaussians.log
+python evalModel_T4.py 2>&1 | tee -a T_1Gaussians.log
 """
 
 
