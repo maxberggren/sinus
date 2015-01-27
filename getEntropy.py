08a4f9cab480eb2d77dd54f790f015780c280736
Max Berggren <maxberggren@gmail.com>
2015-01-27 12:48:56 +0100

    MVP test

--- getEntropy.py
+++ getEntropy.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     try:
         token = sys.argv[1]
         print type(token)
-        tokenQ = "AND token = '{token}' ".format(token=token)
+        tokenQ = "token = '{token}' ".format(token=token)
     except:
         tokenQ = "entropy is NULL "
         
