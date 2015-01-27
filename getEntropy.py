97ce7e4116dab15eda86300725c0dcf403821559
Max Berggren <maxberggren@gmail.com>
2015-01-27 13:15:31 +0100

    MVP test

--- getEntropy.py
+++ getEntropy.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
                                    "FROM posts INNER JOIN blogs ON "
                                    "blogs.id=posts.blog_id "
                                    "WHERE MATCH(posts.text) "
-                                   "AGAINST ('" + searchWord + "' "
+                                   "AGAINST ('" + searchWord.encode('utf-8') + "' "
                                    "IN BOOLEAN MODE) "
                                    "AND blogs.latitude is not NULL "
                                    "AND blogs.longitude is not NULL "
