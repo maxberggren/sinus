8f7b9cfd3af16c73791b355c0f9790944fdbd4e7
Max Berggren <maxberggren@gmail.com>
2015-02-26 15:54:10 +0100

    shapefiles i guit

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -217,10 +217,10 @@ def genShapefileImg(data, words, zoom, binThreshold, emptyBinFallback):
     lds = pd.concat(lds)
     
     if zoom:
-        llcrnrlon = lds['longitude'].quantile(0.03)
-        llcrnrlat = lds['latitude'].quantile(0.03)
-        urcrnrlon = lds['longitude'].quantile(0.99)
-        urcrnrlat = lds['latitude'].quantile(0.99)
+        llcrnrlon = lds['longitude'].quantile(0.10)
+        llcrnrlat = lds['latitude'].quantile(0.10)
+        urcrnrlon = lds['longitude'].quantile(0.94)
+        urcrnrlat = lds['latitude'].quantile(0.94)
     else:
         llcrnrlon = 8
         llcrnrlat = 54.5
