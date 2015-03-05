397ddd49c8851b945644900753f60da47a7c871a
Max Berggren <maxberggren@gmail.com>
2015-03-05 13:16:40 +0100

    byod

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -1207,12 +1207,12 @@ def byod():
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     
     if request.method == 'POST':
-    file = request.files['file']
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('uploaded_file',
-                                filename=filename))
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
            
     return render_template("byod.html", data=None)
 
