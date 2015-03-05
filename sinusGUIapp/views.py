8b4e92967e805b638ffbd763821a912824e34757
Max Berggren <maxberggren@gmail.com>
2015-03-05 13:31:32 +0100

    byod

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -1214,10 +1214,9 @@ def byod():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            print "sddasasdddasads!"
             print filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
+            return redirect(url_for('byod',
                                     filename=filename))
            
     return render_template("byod.html", data=None)
