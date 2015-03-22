39e8b1dcaf951671ab4bc8b7f457de857a234dd4
Max Berggren <maxberggren@gmail.com>
2015-03-22 19:49:11 +0100

    2ES

--- mysql2es.py
+++ mysql2es.py
@@ -10,11 +10,15 @@ import urllib2
 import json
 import datetime
 import config as c
-
+#from elasticsearch import Elasticsearch
+#from elasticsearch import helpers
 
 if __name__ == "__main__":
     documents = dataset.connect(c.LOCATIONDB)
     documents.query("set names 'utf8';")
+
+    #es = Elasticsearch()
+
     i, j, k = 0, 0, 0
     try:
         
@@ -24,31 +28,62 @@ if __name__ == "__main__":
             print "Hittade " + str(row['c']) + " st källor." 
             sources = row['c']
         
-        print "Inserting blogs"
+        print "Lägger in bloggar en efter en som dokument i ES."
         rows = []
         
         # Blogs
-        for source in documents.query("SELECT * from blogs"):                         
+        for source in documents.query("SELECT * from blogs"):   
+            actions = []                      
             j += 1
             k += 1
-            url = source['url']   
+            url = source['url']  
+            blogid = source['id']   
             print url
-            """
-            blog = dict(id=source['rowid'],
-                        url=source[UNIQUENAME], 
-                        city=source['Ort'],
-                        municipality=source['Kommun'],
-                        county=source['Ln'], 
-                        country=source['Land'],
-                        intrests=source['Intressen'],
-                        presentation=source['text'],
-                        gender='',
-                        source=SOURCE,
-                        rank=2)
-            """
-                        
-            #rows.append(blog)
-           
+
+            for post in documents.query("SELECT * from posts "
+                                         "WHERE blog_id = " + str(blogid): 
+                
+                i += 1
+                # date, text, blog_id, posttitle, posturl
+
+
+                document = dict(url=url, 
+                                blogid=blogid,
+
+                                city=source['city'],
+                                country=source['country'],
+                                municipality=source['municipality'],
+                                county=source['county'],
+                                intrests=source['intrests'],
+                                presentation=source['presentation'],
+                                longitude=source['longitude'],
+
+                                latitude=source['latitude'],
+                                longitude=source['longitude'],
+
+                                gender=source['gender'],
+                                age=source['age'],
+                                rank=source['rank'],
+                                source=source['source'],
+
+                                text=post['text'],
+                                date=post['date'],
+                                blog_id=post['blog_id'],
+                                posttitle=post['posttitle'],
+                                posturl=post['posturl']
+                                )
+
+
+                action = {
+                    "_index": "sinus-index",
+                    "_type": "sinus-data",
+                    "_id": i,
+                    "_source": document
+                    }
+
+                actions.append(action)
+
+            #helpers.bulk(es, actions)
     
     except KeyboardInterrupt:
         print "Avbryter..."
