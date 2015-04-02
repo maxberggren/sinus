658a5dc20f599bb6743c106a7ac20dd17377f408
Max Berggren <maxberggren@gmail.com>
2015-04-02 09:51:59 +0200

    chi2

--- chi2test.py
+++ chi2test.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
             coordinates.append([source['longitude'], source['latitude']])
     
-        print coordiantes
+        print coordinates
     
     except KeyboardInterrupt:
         print "Avbryter..."
