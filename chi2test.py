c4d505851efa4ae49913791afbc4f31b88dd2203
Max Berggren <maxberggren@gmail.com>
2015-04-02 10:47:36 +0200

    chi2

--- chi2test.py
+++ chi2test.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
 
             coordinates.append([source['longitude'], source['latitude']])
             
-            if j % 1000:
+            if j % 300:
                 diff = old_matrix - normalize(genGrid(coordinates)) 
                 diff = np.square(diff)
                 total_error = sum1(diff)
