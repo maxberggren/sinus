0807b1dcff78a28d2b799f322aba94731b0258e9
Max Berggren <maxberggren@gmail.com>
2015-04-09 11:07:39 +0200

    refactorisera

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -200,6 +200,7 @@ def genShapefileImg(data, words, zoom, binThreshold, emptyBinFallback):
         colorbar.set_ticks(np.linspace(0, ncolors, ncolors+1)+0.5)
         colorbar.set_ticklabels(range(0, ncolors))
         colorbar.set_ticklabels(labels)
+        
         return colorbar
     
     def opacify(cmap):
@@ -208,6 +209,76 @@ def genShapefileImg(data, words, zoom, binThreshold, emptyBinFallback):
         alphas = np.abs(np.linspace(0, 1.0, cmap.N))
         cmap._lut[:-3,-1] = alphas
         return cmap
+        
+    def genGrid(koordinater, xBins=10, xyRatio=1.8):
+    
+        if len(koordinater) == 0:
+            return np.zeros(shape=(int(xBins*xyRatio-1), xBins-1))
+            
+        lon_bins = np.linspace(8, 26, xBins)
+        lat_bins = np.linspace(54.5, 69.5, xBins*xyRatio)
+    
+        lons, lats = zip(*koordinater)             
+        lons = np.array(lons)
+        lats = np.array(lats)
+    
+        density, _, _ = np.histogram2d(lats, 
+                                       lons, 
+                                       [lat_bins, 
+                                        lon_bins])
+        return density
+    
+    def sum1(input):
+        try:
+            return sum(map(sum, input))
+        except Exception:
+            return sum(input)
+        
+    def normalize(matrix):
+        return matrix / sum1(matrix)        
+        
+    def getEnoughData():
+        old_matrix = genGrid([])
+        i, j, k = 0, 0, 0
+        try:        
+            coordinates = []
+            result = documents.query("SELECT count(*) as c "
+                                     "from blogs "
+                                     "WHERE longitude is not NULL and "
+                                     "latitude is not NULL")
+            for row in result:
+                sources = row['c']
+                print "Hittade {} st källor.".format(sources)
+                        
+            # Blogs
+            for source in documents.query("SELECT * from blogs "
+                                          "WHERE longitude is not NULL and "
+                                          "latitude is not NULL "
+                                          "ORDER BY RAND() "):   
+                j += 1
+                k += 1
+                url = source['url']  
+                blogid = source['id']    
+    
+                coordinates.append([source['longitude'], source['latitude']])
+                
+                if j % 1000:
+                    diff = old_matrix - normalize(genGrid(coordinates)) 
+                    diff = np.square(diff)
+                    total_error = sum1(diff)
+                    
+                    if total_error != 0.0:
+                        print total_error
+                        print j
+                        
+                    if total_error < 1e-6 and total_error != 0.0:
+                       return coordinates
+                       break
+                    
+                    old_matrix = normalize(genGrid(coordinates))
+                    
+        except KeyboardInterrupt:
+            print "Avbryter..."        
 
 
     lds, coord_count = [], {}
@@ -218,9 +289,12 @@ def genShapefileImg(data, words, zoom, binThreshold, emptyBinFallback):
         ld['word'] = word
         lds.append(ld)
     
+    # data = [[lon, lat], [lon, lat]]
+    
     lds = pd.concat(lds)
     
     padding = 0.5
+    
     if zoom:
         llcrnrlon = lds['longitude'].quantile(0.05) - padding
         llcrnrlat = lds['latitude'].quantile(0.20) - padding
@@ -280,7 +354,7 @@ def genShapefileImg(data, words, zoom, binThreshold, emptyBinFallback):
     
     
     def mapPointsToPoly(coordinates_df, poly_df):
-        """ Take coordiantes DF and put into polygon DF """
+        """ Take coordiates DF and put into polygon DF """
         
         mapped_points, all_points, hood_polygons = {}, {}, {}
         
@@ -307,11 +381,20 @@ def genShapefileImg(data, words, zoom, binThreshold, emptyBinFallback):
         for word in words:
             poly_df[word] = poly_df['poly'].apply(num_of_contained_points, 
                                                   args=(mapped_points[word],))
-        
         return poly_df
+        
         
     df_map_muni = mapPointsToPoly(lds, df_map_muni)
     df_map_county = mapPointsToPoly(lds, df_map_county)
+    
+    # Get null hypothesis if its only one word
+    if len(words) == 1:
+        # data = [[lon, lat], [lon, lat]]
+        #ld = pd.DataFrame(d, columns=['longitude', 'latitude'])
+        #ld['word'] = "null_hypothesis"
+        data = getEnoughData()
+        #null_h_coordinates_df = getNullHypothesis()
+        #df_map_null_hypothesis = mapPointsToPoly(null_h_coordinates_df, df_map_county)
         
     # Get total occurencies in every county/municipality
     df_map_county["sum"] = df_map_county[words].sum(axis=1)
