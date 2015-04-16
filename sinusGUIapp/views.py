d9aad542aa395d66e6d57a045a73873f0c9e016f
Max Berggren <maxberggren@gmail.com>
2015-04-16 11:12:14 +0200

    fallback: parkvall deluxe

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -367,17 +367,17 @@ def genShapefileImg(data, ranks, words, zoom, binThreshold, emptyBinFallback):
             
             for rank, ld in lds.groupby(['rank']):
                 # Convert our latitude and longitude into Basemap cartesian map coordinates
-                mapped_points[word] += [Point(m(mapped_x, mapped_y)) 
+                mapped_points[word] = [Point(m(mapped_x, mapped_y)) 
                                        for mapped_x, mapped_y 
                                        in zip(ld['longitude'], ld['latitude'])]
                                        
-                all_points[word] += MultiPoint(mapped_points[word])
+                all_points[word] = MultiPoint(mapped_points[word])
             
                 # Use prep to optimize polygons for faster computation
-                hood_polygons[word] += prep(MultiPolygon(list(poly_df['poly'].values)))
+                hood_polygons[word] = prep(MultiPolygon(list(poly_df['poly'].values)))
             
                 # Filter out the points that do not fall within the map we're making
-                mapped_points[word] += filter(hood_polygons[word].contains, all_points[word])
+                mapped_points[word] = filter(hood_polygons[word].contains, all_points[word])
         
         
         def num_of_contained_points(apolygon, mapped_points):
