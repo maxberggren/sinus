cb55c1df97ed7e9909e4ceb9b613f618bf5d7dd6
Max Berggren <maxberggren@gmail.com>
2015-04-16 15:34:37 +0200

    fallback: parkvall deluxe

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -245,7 +245,7 @@ def genShapefileImg(data, ranks, words, zoom, binThreshold, binModel):
     def getEnoughData():
         """ Get alot of data until a suitable null hypothesis has converged """
         
-        convergenceCrit = 1e-6 
+        convergenceCrit = 1e-9 
         old_matrix = genGrid([])
         i, j, k = 0, 0, 0
         try:        
