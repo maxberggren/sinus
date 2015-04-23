c7a967b16b79eaf8d0ef9b91cc4633922936e1b2
Max Berggren <maxberggren@gmail.com>
2015-04-23 13:26:31 +0200

    fallback: parkvall deluxe

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -561,10 +561,8 @@ def genShapefileImg(data, ranks, words, zoom, binThreshold, binModel):
                                                                           zorder=4))
             pc = PatchCollection(temp_df['patches'], match_original=True)
             # Apply our custom color values onto the patch collection
-            cmaps = (df_map['jenks_bins_'+word].values - 
-                       df_map['jenks_bins_'+word].values.min())/(
-                           df_map['jenks_bins_'+word].values.max()-
-                               float(df_map['jenks_bins_'+word].values.min()))
+            cmaps = (temp_df['jenks_bins_'+word].values - 1)/(
+                           temp_df['jenks_bins_'+word].values.max()-float(1))
                                
             cmap_list = [cmap(val) for val in cmaps]
             pc.set_facecolor(cmap_list)
