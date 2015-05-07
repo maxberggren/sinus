97f6ea4c8fbcc44bce6f063af067efba7e8d5113
Max Berggren <maxberggren@gmail.com>
2015-05-07 10:36:57 +0200

    fallback: parkvall deluxe

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -532,7 +532,7 @@ def genShapefileImg(data, ranks, words, zoom, binThreshold, binModel):
                 parent = hierarchy.loc[hierarchy[u'Kommun'] == municipality][level].values[0]
                 if not parent == "-":
                     munis = getMuni(hierarchy, level, parent)
-                    return np.mean(df.loc[df['name'].isin(munis)][word])
+                    return np.mean(df.loc[df['name'].isin(munis), word])
                 else:
                     return None
             except IndexError:
