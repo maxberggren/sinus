0579913564ff13debe227f200c485365e9d44273
Max Berggren <maxberggren@gmail.com>
2015-05-28 15:30:22 +0200

    count words to find interesting words

---
+++ wordcounts2df.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import config as c
+
+engine = create_engine(c.LOCATIONDB, echo=False)
+
+df = pd.read_sql_query('SELECT * FROM wordcounts', engine, index_col='id')
