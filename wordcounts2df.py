7652f81289279f0c6f50a1220000188f497950a0
Max Berggren <maxberggren@gmail.com>
2015-05-28 15:44:41 +0200

    count words to find interesting words

--- wordcounts2df.py
+++ wordcounts2df.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from sqlalchemy import create_engine
 import pandas as pd
 import config as c
@@ -9,7 +10,8 @@ df = pd.read_sql_query('SELECT * FROM wordcounts', engine, index_col='id')
 print df.head()
 
 def rel_error(values):
-    return len(values)
+    if len(values) == 2:
+        return abs((values[0] - values[1])/values[0])
 
 grouped_count = df.groupby("token").frequency.agg(rel_error)
 
