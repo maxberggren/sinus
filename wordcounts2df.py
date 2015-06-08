8e221cf0667d9f923c08d78b50eb07765891d3fc
Max Berggren <maxberggren@gmail.com>
2015-06-08 19:36:39 +0200

    wordcount

--- wordcounts2df.py
+++ wordcounts2df.py
@@ -7,6 +7,7 @@ import config as c
 engine = create_engine(c.LOCATIONDB, echo=False)
 
 df = pd.read_sql_query('SELECT * FROM wordcounts', engine, index_col='id')
+df = df[df['frequency'] > 100]
 print df.head()
 
 """
