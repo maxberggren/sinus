2f713e7b358a17e97956b7c48043ec268360938f
Max Berggren <maxberggren@gmail.com>
2015-06-08 18:59:41 +0200

    wordcount

--- wordcounts2df.py
+++ wordcounts2df.py
@@ -20,4 +20,4 @@ def rel_error(values):
 
 grouped_count = df.groupby("token").frequency.agg(rel_error)
 
-print grouped_count.order(ascending=False).values()[0:300]
+print grouped_count.order(ascending=False).values[0:300]
