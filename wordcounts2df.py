bb214a40fa32aa847cd8268b79dfd7ceb6bd193e
Max Berggren <maxberggren@gmail.com>
2015-06-09 13:16:12 +0200

    wordcount

--- wordcounts2df.py
+++ wordcounts2df.py
@@ -25,6 +25,6 @@ grouped_count = df.groupby("token").frequency.agg(rel_frq)
 
 i = 0
 for index, value in grouped_count.order(ascending=False).iteritems():
-    print index, value, index.decode('utf-8')
+    print index, value
     if value < 0.5:
         break
