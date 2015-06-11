1a95952939cd7d569d855d298a2a8d64c86c411a
Max Berggren <maxberggren@gmail.com>
2015-06-11 10:49:54 +0200

    tweak matrix multiplication

--- genGrids.py
+++ genGrids.py
@@ -118,8 +118,9 @@ def get_grids(queries, xBins=15):
         print "letar efter {} i {}".format(word, source)
         
         grid = cache.get(str(query) + str(xBins))
-        print type(grid) 
+
         if isinstance(grid, np.ndarray): # Found in cache
+            print "hämtade från cachen: {}".format(str(query) + str(xBins))
             grids.append(grid)
             
         else: # Not found in cache
