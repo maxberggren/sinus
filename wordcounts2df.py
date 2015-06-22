67f14ace78164505335e6e5f80263fb9bad92f6c
Max Berggren <maxberggren@gmail.com>
2015-06-22 12:32:21 +0200

    wordcount

--- wordcounts2df.py
+++ wordcounts2df.py
@@ -9,7 +9,7 @@ check_region = "skaune"
 df = pd.read_sql_query('SELECT * FROM wordcounts '
                        'WHERE region = "country" '
                        'or region = "' + check_region + '"' 
-                       'and frequency > 20', 
+                       'and frequency > 40', 
                        engine, index_col='id')
 
 #df = df[df['frequency'] > 10]
