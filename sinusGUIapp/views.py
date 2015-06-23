8196d9cfb6ee2ade1b5525d31b3bcb82d4364c1d
Max Berggren <maxberggren@gmail.com>
2015-06-23 21:05:06 +0200

    finland

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -698,7 +698,7 @@ def genShapefileImg(data, ranks, words, zoom, binThreshold, binModel):
             pc.set_facecolor(cmap_list)
             ax.add_collection(pc)
             
-        #m.drawcoastlines(linewidth=0.5) 
+        m.drawcoastlines(linewidth=0.25) 
         m.drawcountries()
         m.drawstates()
         m.drawmapboundary()
@@ -897,7 +897,7 @@ def genGridImg(coordinatesByWord, xBins, words, zoom,
                         urcrnrlon=urcrnrlon, 
                         urcrnrlat=urcrnrlat,)   
             
-            #m.drawcoastlines(linewidth=0.5)
+            m.drawcoastlines(linewidth=0.25)
             m.drawcountries()
             m.drawmapboundary()
             m.fillcontinents(color='white',
