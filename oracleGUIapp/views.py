81975163a8fbce63a731f9d8e987ce977041cd01
Max Berggren <maxberggren@gmail.com>
2015-06-25 11:55:28 +0200

    adding the oracle GUI

--- oracleGUIapp/views.py
+++ oracleGUIapp/views.py
@@ -5,12 +5,12 @@ from oracleGUIapp import app
 from flask import Flask, jsonify, make_response, request, render_template, redirect, url_for
 from werkzeug import secure_filename
 
-questions = [('Sluddrar och pratar du grötigt?', 
-              'Detta är en undertext som _förklarar mer_.', 
+questions = [(u'Sluddrar och pratar du grötigt?', 
+              u'Detta är en undertext som _förklarar mer_.', 
               ['Ja', 'Nej']),
               
-             ('Fara eller åka?',
-              'Använder du ibland fara som i _vi skulle vara till farmor_?',
+             (u'Fara eller åka?',
+              u'Använder du ibland fara som i _vi skulle vara till farmor_?',
               ['Jo', 'Nej'])]
 
 @app.route('/oracle/', methods = ['GET', 'POST'])
