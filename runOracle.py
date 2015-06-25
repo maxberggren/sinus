8a03d3c6fc8be06a785766955e77ecd8845e9064
Max Berggren <maxberggren@gmail.com>
2015-06-25 14:04:15 +0200

    adding the oracle GUI

--- runOracle.py
+++ runOracle.py
@@ -3,8 +3,10 @@
 import matplotlib  
 matplotlib.use('Agg')
 from oracleGUIapp import app
-from flask.ext.markdown import Markdown
-md = Markdown(app, extensions=['fenced_code'])
+
+@app.template_filter('italic')
+def reverse_filter(s):
+    return s[::-1]
 
 app.debug = True
 app.run(host='0.0.0.0', port=5009)
