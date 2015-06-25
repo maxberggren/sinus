4da5bd9a5c3fcb9f1adaf8b7e1ee4526b0b5ff6f
Max Berggren <maxberggren@gmail.com>
2015-06-25 14:05:08 +0200

    adding the oracle GUI

--- runOracle.py
+++ runOracle.py
@@ -3,10 +3,11 @@
 import matplotlib  
 matplotlib.use('Agg')
 from oracleGUIapp import app
+import re
 
 @app.template_filter('italic')
 def reverse_filter(s):
-    return s[::-1]
+    return re.sub('\*{2}(.+)\*{2}', '<i>\\1</i>', s)
 
 app.debug = True
 app.run(host='0.0.0.0', port=5009)
