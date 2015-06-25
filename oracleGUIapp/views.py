190f3385785d63d479ec8361cdc2d71a6121c6eb
Max Berggren <maxberggren@gmail.com>
2015-06-25 14:07:46 +0200

    adding the oracle GUI

--- oracleGUIapp/views.py
+++ oracleGUIapp/views.py
@@ -6,11 +6,11 @@ from flask import Flask, jsonify, make_response, request, render_template, redir
 from werkzeug import secure_filename
 
 questions = [(u'Sluddrar och pratar du grötigt?', 
-              u'Detta är en undertext som <em>förklarar mer</em>.', 
+              u'Detta är en undertext som **förklarar mer**.', 
               ['Ja', 'Nej']),
               
              (u'Fara eller åka?',
-              u'Använder du ibland fara som i <em>vi skulle vara till farmor</em>?',
+              u'Använder du ibland fara som i **vi skulle vara till farmor**?',
               ['Jo', 'Nej']),
               
              (u'Polisen?',
