0b7e07babf857ef7b44cb4862b7c25273866b84c
Max Berggren <maxberggren@gmail.com>
2015-06-25 11:30:51 +0200

    adding the oracle GUI

--- oracleGUIapp/views.py
+++ oracleGUIapp/views.py
@@ -5,11 +5,9 @@ from oracleGUIapp import app
 from flask import Flask, jsonify, make_response, request, render_template, redirect, url_for
 from werkzeug import secure_filename
 
-print "wwaaaat"
 @app.route('/oracle/', methods = ['GET', 'POST'])
 @app.route('/oracle', methods = ['GET', 'POST'])
 def oracle():
-    print "wat"
     return render_template("index.html")
 
     
