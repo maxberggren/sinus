b7b1525c420cc04cb70211057f848efb7bb82094
Max Berggren <maxberggren@gmail.com>
2015-07-09 19:55:57 +0200

    ny prot

--- oracleGUIapp/views.py
+++ oracleGUIapp/views.py
@@ -5,24 +5,31 @@ from oracleGUIapp import app
 from flask import Flask, jsonify, make_response, request, render_template, redirect, url_for
 from werkzeug import secure_filename
 
-questions = [{'q': u'Sluddrar och pratar du grötigt?', 
-              'sq': u'Detta är en undertext som **förklarar mer**.', 
-              'alt': ['Ja', 'Nej'], 
+questions = [{'question': u'Sluddrar och pratar du grötigt?', 
+              'explanation': u'Detta är en undertext som **förklarar mer**.', 
+              'answers': ['Ja', 'Nej'], 
               'id': 1},
               
-             {'q': u'Fara eller åka?',
-              'sq': u'Fyll i följande mening: **vi skulle...**',
-              'alt': ['...fara till farmor', u'...åka till farmor'], 
+             {'question': u'Fara eller åka?',
+              'explanation': u'Fyll i följande mening: **vi skulle...**',
+              'answers': ['...fara till farmor', u'...åka till farmor'], 
               'id': 2},
               
-             {'q': u'Polisen?',
-              'sq': u'Vilket av följade använder du oftast för att tala om polisen?',
-              'alt': [u'Bängen', 'Snuten', u'Farbror blå'], 
+             {'question': u'Polisen?',
+              'explanation': u'Vilket av följade använder du oftast för att tala om polisen?',
+              'answers': [u'Bängen', 'Snuten', u'Farbror blå'], 
               'id': 3}]
 
 @app.route('/oracle/', methods = ['GET', 'POST'])
 @app.route('/oracle', methods = ['GET', 'POST'])
 def oracle():
     return render_template("index.html", questions=questions, n_questions=len(questions))
+
+
+@app.route('/oracle/predict', methods=['POST'])
+def predict(): 
+    x = request.json['uname']
+                           
+    return jsonify( { 'city1': x )
 
     
