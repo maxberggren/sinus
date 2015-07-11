8c9767e6c6dd400fce05fdfcb573649961ecdd15
Max Berggren <maxberggren@gmail.com>
2015-07-11 21:32:56 +0200

    oracle predict

--- oracleGUIapp/views.py
+++ oracleGUIapp/views.py
@@ -283,8 +283,8 @@ def make_map(matrix, name, coordinate):
  
     # Put maximum on map
     lat, lon = coordinate
-    x1, y1 = m(lat, lon)
-    m.scatter(x1, y1)
+    x1, y1 = m(lon, lat)
+    m.scatter(x1, y1, s=30)
 
     fig.tight_layout(pad=2.5, w_pad=0.1, h_pad=0.0) 
     
