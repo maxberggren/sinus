0164088f24995488c2768f3da053ea04fc5f7a6e
Max Berggren <maxberggren@gmail.com>
2015-07-17 19:09:56 +0200

    oracle predict deviation form mean

--- runOracle.py
+++ runOracle.py
@@ -8,7 +8,7 @@ import re
 @app.template_filter('italic')
 def italic(s):
     my_regex = re.compile(r"\*\*(.+?)\*\*")
-    return my_regex.sub(r'<em>\\1</em>', s)
+    return my_regex.sub(r'<em>\1</em>', s)
 
 app.debug = True
 app.run(host='0.0.0.0', port=5011)
