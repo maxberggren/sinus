aa7eb3d54c952367aaec9aaeec8bbff72638f575
Max Berggren <maxberggren@gmail.com>
2015-07-17 19:45:41 +0200

    oracle predict deviation form mean

--- oracleGUIapp/__init__.py
+++ oracleGUIapp/__init__.py
@@ -4,11 +4,11 @@ import matplotlib
 matplotlib.use('Agg')
 from flask import Flask, jsonify, make_response, request
 
+app = Flask(__name__)
+
 @app.template_filter('italic')
 def italic(s):
     my_regex = re.compile(r"\*\*(.+?)\*\*")
     return my_regex.sub(r'<em>\1</em>', s)
-
-app = Flask(__name__)
 
 from oracleGUIapp import views
