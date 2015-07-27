79d6d5cce8086fcb1b82c93b45404d76548ecc2a
Max Berggren <maxberggren@gmail.com>
2015-07-27 20:22:22 +0200

    hirerachy fix

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -708,7 +708,7 @@ def genShapefileImg(data, ranks, words, zoom, binThreshold, binModel):
             pc.set_facecolor(cmap_list)
             ax.add_collection(pc)
             
-        m.drawcoastlines(linewidth=0.25) 
+        m.drawcoastlines(linewidth=0) 
         m.drawcountries()
         m.drawstates()
         m.drawmapboundary()
@@ -907,7 +907,7 @@ def genGridImg(coordinatesByWord, xBins, words, zoom,
                         urcrnrlon=urcrnrlon, 
                         urcrnrlat=urcrnrlat,)   
             
-            m.drawcoastlines(linewidth=0.25)
+            m.drawcoastlines(linewidth=0)
             m.drawcountries()
             m.drawmapboundary()
             m.fillcontinents(color='white',
