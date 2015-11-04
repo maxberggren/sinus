604bc2e9794b5bb2738a716e0bc793ec1b4faa7f
Max Berggren <maxberggren@gmail.com>
2015-11-04 13:47:21 +0100

    onemap starting now

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -1132,7 +1132,7 @@ def genOneMapShapefileImg(data, ranks, words, zoom, binThreshold, binModel):
             facecolors = []
             for c in cmap_list:
                 if c == 0:
-                    facecolors.append('none')
+                    facecolors.append(cmapOpacity(val, 0))
                 else:
                     facecolors.append(cmap(c))
 
