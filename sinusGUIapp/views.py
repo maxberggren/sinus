01392b72e194e78ba0013eb95ad6cddc041ed6d3
Max Berggren <maxberggren@gmail.com>
2015-11-06 12:39:37 +0100

    kartor onemap

--- sinusGUIapp/views.py
+++ sinusGUIapp/views.py
@@ -735,7 +735,7 @@ def genShapefileImg(data, ranks, words, zoom, binThreshold, binModel, oneMap=Fal
     
 
     if oneMap:      
-        plt.legend(handles=[red_patch], 
+        plt.legend(handles=legends, 
                    bbox_to_anchor=(0., 1.02, 1., .102), 
                    loc=1, 
                    mode="expand", 
