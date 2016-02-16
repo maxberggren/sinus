970d9b865311dbfa2216c2808facbd9e969ebb92
Max Berggren <maxberggren@gmail.com>
2016-02-16 10:31:54 +0100

    metadata

--- metadata2coordinates.py
+++ metadata2coordinates.py
@@ -52,10 +52,10 @@ if __name__ == "__main__":
             for row in result:
                 # Set Nones to empty strings so latlon gets it
                 
-                city = (row['city'] if row['city'] else "").decode('utf-8')
-                muni = (row['municipality'] if row['municipality'] else "").decode('utf-8')
-                county = (row['county'] if row['county'] else "").decode('utf-8')
-                country = (row['country'] if row['country'] else "").decode('utf-8')         
+                city = (row['city'] if row['city'] else "").encode('latin-1')
+                muni = (row['municipality'] if row['municipality'] else "").encode('latin-1')
+                county = (row['county'] if row['county'] else "").encode('latin-1')
+                country = (row['country'] if row['country'] else "").encode('latin-1')         
                                     
                 coordinate = None
         
